afcb4481b425e8d99b5966082a94bcd6c2920c27
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2023-11-19 12:38:29 +0200

    Disable autocorrect on input

--- src/components/ui/TextArea.tsx
+++ src/components/ui/TextArea.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { TextArea as TamaguiTextArea, TextAreaProps } from 'tamagui';
 
 const TextArea = (props: TextAreaProps) => {
-  return <TamaguiTextArea size="$4" borderRadius="$6" lineHeight={17} {...props} />;
+  return (
+    <TamaguiTextArea size="$4" borderRadius="$6" autoCorrect={false} lineHeight={17} {...props} />
+  );
 };
 
 export default TextArea;
