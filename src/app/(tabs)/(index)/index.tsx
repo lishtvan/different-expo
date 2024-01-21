347706bf94a6b432ebbc109f48beefc5a7788e7e
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-01-21 13:41:30 +0200

    Implement filters count on home page

--- src/app/(tabs)/(index)/index.tsx
+++ src/app/(tabs)/(index)/index.tsx
@@ -1,9 +1,9 @@
 import { EvilIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { Stack, router } from 'expo-router';
 import React from 'react';
-import { useInstantSearch } from 'react-instantsearch-core';
+import { useCurrentRefinements, useInstantSearch } from 'react-instantsearch-core';
 import { Dimensions, RefreshControl, TouchableOpacity } from 'react-native';
-import { Button, Input, View, XStack } from 'tamagui';
+import { Button, Input, Text, View, XStack } from 'tamagui';
 
 import HomeListings from '../../../components/home/Listings';
 import { useRefresh } from '../../../hooks/useRefresh';
@@ -33,47 +33,60 @@ const HomeListingsWrapper = () => {
   );
 };
 
-const HomeScreen = () => {
+const Header = () => {
   const ScreenWidth = Dimensions.get('window').width;
   const iconClassname = isAndroid ? 'p-0 pl-2 pb-1' : 'p-0 pl-2';
 
+  const { items } = useCurrentRefinements({ excludedAttributes: ['status'] });
+
+  return (
+    <Stack.Screen
+      options={{
+        contentStyle: { paddingTop: 7 },
+        headerTitle: () => (
+          <XStack alignItems="center" style={{ width: ScreenWidth - 30 }}>
+            <Button
+              size="$4"
+              icon={<EvilIcons name="search" size={30} />}
+              className={iconClassname}
+              borderTopLeftRadius="$main"
+              borderBottomLeftRadius="$main"
+              borderTopRightRadius="$0"
+              backgroundColor="#f8f4f4"
+              borderBottomRightRadius="$0"
+              borderWidth="$0"
+              borderRightWidth="$0"
+            />
+            <Input
+              autoCorrect={false}
+              placeholder="Пошук"
+              backgroundColor="#f8f4f4"
+              flex={1}
+              paddingLeft={6}
+              borderWidth="$0"
+              borderRadius="$0"
+              borderTopRightRadius="$main"
+              borderBottomRightRadius="$main"
+            />
+            <TouchableOpacity className="ml-1 p-2 flex-row" onPress={() => router.push('/filters')}>
+              <MaterialCommunityIcons name="tune-variant" size={25} />
+              {items.length > 0 && (
+                <View className="bg-main px-2 ml-2 flex-row items-center justify-center rounded-full">
+                  <Text className="text-white">{items.length}</Text>
+                </View>
+              )}
+            </TouchableOpacity>
+          </XStack>
+        ),
+      }}
+    />
+  );
+};
+
+const HomeScreen = () => {
   return (
     <View>
-      <Stack.Screen
-        options={{
-          contentStyle: { paddingTop: 7 },
-          headerTitle: () => (
-            <XStack alignItems="center" style={{ width: ScreenWidth - 30 }}>
-              <Button
-                size="$4"
-                icon={<EvilIcons name="search" size={30} />}
-                className={iconClassname}
-                borderTopLeftRadius="$main"
-                borderBottomLeftRadius="$main"
-                borderTopRightRadius="$0"
-                backgroundColor="#f8f4f4"
-                borderBottomRightRadius="$0"
-                borderWidth="$0"
-                borderRightWidth="$0"
-              />
-              <Input
-                autoCorrect={false}
-                placeholder="Пошук"
-                backgroundColor="#f8f4f4"
-                flex={1}
-                paddingLeft={6}
-                borderWidth="$0"
-                borderRadius="$0"
-                borderTopRightRadius="$main"
-                borderBottomRightRadius="$main"
-              />
-              <TouchableOpacity className="ml-1 p-2" onPress={() => router.push('/filters')}>
-                <MaterialCommunityIcons name="tune-variant" size={25} />
-              </TouchableOpacity>
-            </XStack>
-          ),
-        }}
-      />
+      <Header />
       <HomeListingsWrapper />
     </View>
   );
