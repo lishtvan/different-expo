6161e08666d56c63089d0691493f5f4f41f899b6
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-01-26 22:13:33 +0200

    Profile perf improvements

--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -2,7 +2,7 @@ import { EvilIcons } from '@expo/vector-icons';
 import { useScrollToTop } from '@react-navigation/native';
 import { useQuery } from '@tanstack/react-query';
 import { router, useLocalSearchParams } from 'expo-router';
-import React, { FC, useEffect, useRef } from 'react';
+import React, { FC, memo, useEffect, useRef } from 'react';
 import {
   useInfiniteHits,
   useInstantSearch,
@@ -72,20 +72,108 @@ const renderItem = ({ item }: { item: TListing }) => {
   );
 };
 
-const UserContent: FC<Props> = ({ refreshControl, user }) => {
-  const scrollRef = useRef(null);
-  useScrollToTop(scrollRef);
-
-  const { hits, isLastPage, showMore } = useInfiniteHits<TListing>();
+const StatusSwitcher = () => {
   const { refine, value } = useToggleRefinement({
     attribute: 'status',
     on: 'SOLD',
     off: 'AVAILABLE',
   });
+
+  return (
+    <View className="flex-row items-center mb-4 mt-1 px-3 gap-x-4">
+      <Text className="text-lg">Показати продані речі</Text>
+      <Switch
+        size="$3"
+        checked={value.isRefined}
+        backgroundColor={value.isRefined ? mainColor : '$gray7'}
+        onCheckedChange={() => refine({ isRefined: value.isRefined })}>
+        <Switch.Thumb backgroundColor="white" animation="bouncy" />
+      </Switch>
+    </View>
+  );
+};
+
+const MyHeader = ({ user }: { user: TUser }) => {
+  return (
+    <View>
+      <View className="flex flex-row gap-x-4 px-2 ">
+        <Avatar circular size="$10">
+          <Avatar.Image
+            src={
+              user.avatarUrl ||
+              'https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg'
+            }
+          />
+        </Avatar>
+        <View className="flex flex-row items-center gap-x-6">
+          <View className="flex items-center">
+            <Text className="font-bold" fontSize="$6">
+              {user.availableListingsCount}
+            </Text>
+            <Text className="font-medium" fontSize="$6">
+              оголошень
+            </Text>
+          </View>
+          <View className="flex items-center">
+            <Text className="font-bold" fontSize="$6">
+              {user.soldListingsCount}
+            </Text>
+            <Text className="font-medium" fontSize="$6">
+              продано
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        {user.location && (
+          <View className="mt-2 flex-row items-center px-1.5">
+            <EvilIcons size={25} style={{ padding: 0, opacity: 0.6 }} name="location" />
+            <Text opacity={0.6} fontSize="$6">
+              {user.location}
+            </Text>
+          </View>
+        )}
+        {user.bio && (
+          <Text fontSize="$6" className="mt-2 px-3">
+            {user.bio.replace(/\n+/g, '\n')}
+          </Text>
+        )}
+      </View>
+      <View className="mb-3 mt-4 w-full flex-row items-center gap-x-4 px-2">
+        <Button
+          onPress={() => router.push(user.isOwnAccount ? '/(tabs)/profile/settings' : '/')}
+          size="$3"
+          theme="active"
+          className="w-[47%]"
+          fontSize="$5"
+          borderRadius="$main">
+          {user.isOwnAccount ? 'Редагувати' : 'Повідомлення'}
+        </Button>
+        <Button
+          onPress={() => shareLink(user.nickname)}
+          size="$3"
+          className="w-[47%]"
+          fontSize="$5"
+          borderRadius="$main">
+          Поділитися
+        </Button>
+      </View>
+      {user.soldListingsCount > 0 && <StatusSwitcher />}
+    </View>
+  );
+};
+
+const Header = memo(MyHeader);
+
+const UserContent: FC<Props> = ({ refreshControl, user }) => {
+  const scrollRef = useRef(null);
+  useScrollToTop(scrollRef);
+
+  const { hits, isLastPage, showMore } = useInfiniteHits<TListing>();
+
   const { refine: refineSeller } = useRefinementList({ attribute: 'sellerId' });
 
   useEffect(() => {
-    console.log('refine');
     refineSeller(user.id.toString());
   }, []);
 
@@ -95,86 +183,7 @@ const UserContent: FC<Props> = ({ refreshControl, user }) => {
         ref={scrollRef}
         data={hits}
         refreshControl={refreshControl}
-        keyboardDismissMode="on-drag"
-        ListHeaderComponent={() => (
-          <View>
-            <View className="flex flex-row gap-x-4 px-2 ">
-              <Avatar circular size="$10">
-                <Avatar.Image
-                  src={
-                    user.avatarUrl ||
-                    'https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg'
-                  }
-                />
-              </Avatar>
-              <View className="flex flex-row items-center gap-x-6">
-                <View className="flex items-center">
-                  <Text className="font-bold" fontSize="$6">
-                    {user.availableListingsCount}
-                  </Text>
-                  <Text className="font-medium" fontSize="$6">
-                    оголошень
-                  </Text>
-                </View>
-                <View className="flex items-center">
-                  <Text className="font-bold" fontSize="$6">
-                    {user.soldListingsCount}
-                  </Text>
-                  <Text className="font-medium" fontSize="$6">
-                    продано
-                  </Text>
-                </View>
-              </View>
-            </View>
-
-            <View>
-              {user.location && (
-                <View className="mt-2 flex-row items-center px-1.5">
-                  <EvilIcons size={25} style={{ padding: 0, opacity: 0.6 }} name="location" />
-                  <Text opacity={0.6} fontSize="$6">
-                    {user.location}
-                  </Text>
-                </View>
-              )}
-              {user.bio && (
-                <Text fontSize="$6" className="mt-2 px-3">
-                  {user.bio.replace(/\n+/g, '\n')}
-                </Text>
-              )}
-            </View>
-            <View className="mb-3 mt-4 w-full flex-row items-center gap-x-4 px-2">
-              <Button
-                onPress={() => router.push(user.isOwnAccount ? '/(tabs)/profile/settings' : '/')}
-                size="$3"
-                theme="active"
-                className="w-[47%]"
-                fontSize="$5"
-                borderRadius="$main">
-                {user.isOwnAccount ? 'Редагувати' : 'Повідомлення'}
-              </Button>
-              <Button
-                onPress={() => shareLink(user.nickname)}
-                size="$3"
-                className="w-[47%]"
-                fontSize="$5"
-                borderRadius="$main">
-                Поділитися
-              </Button>
-            </View>
-            {user.soldListingsCount === 0 && (
-              <View className="flex-row items-center mb-4 mt-1 px-3 gap-x-4">
-                <Text className="text-lg">Показати продані</Text>
-                <Switch
-                  size="$3"
-                  checked={value.isRefined}
-                  backgroundColor={value.isRefined ? mainColor : '$gray7'}
-                  onCheckedChange={() => refine({ isRefined: value.isRefined })}>
-                  <Switch.Thumb backgroundColor="white" animation="bouncy" />
-                </Switch>
-              </View>
-            )}
-          </View>
-        )}
+        ListHeaderComponent={<Header user={user} />}
         onEndReached={() => {
           if (!isLastPage) showMore();
         }}
