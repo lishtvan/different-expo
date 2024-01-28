7f690e6239b39e5d7088d56a7a39d8681a9d46b3
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-01-28 18:59:14 +0200

    Revert "Dependencies update: 4th iteration (non-breaking probably)"
    This reverts commit f2043c25293f43609f621b0846b286aa799303a9.

--- src/app/(tabs)/sell/index.tsx
+++ src/app/(tabs)/sell/index.tsx
@@ -157,7 +157,7 @@ export default function SellScreen() {
     <KeyboardAwareScrollView
       viewIsInsideTabBar
       keyboardShouldPersistTaps="handled"
-      extraScrollHeight={100}
+      extraScrollHeight={25}
       className="flex-1 gap-y-3 p-3">
       <View>
         <Text className="mb-1 ml-2 text-base">Заголовок *</Text>
@@ -616,7 +616,7 @@ export default function SellScreen() {
         theme="active"
         fontSize="$6"
         borderRadius="$main"
-        className="mb-20 mt-4">
+        className="mb-12">
         Створити
       </Button>
     </KeyboardAwareScrollView>
