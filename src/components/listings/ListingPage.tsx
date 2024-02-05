9328f76fcb32339b6f606f71835039c5b91b98f9
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-02-05 18:06:18 +0200

    Listing page: finish carousel with zoom

--- src/components/listings/ListingPage.tsx
+++ src/components/listings/ListingPage.tsx
@@ -2,34 +2,41 @@ import { Image } from 'expo-image';
 import { useState } from 'react';
 import { Dimensions } from 'react-native';
 import AnimatedDotsCarousel from 'react-native-animated-dots-carousel';
+// @ts-expect-error pwdq
+import Pinchable from 'react-native-pinchable';
 import Carousel from 'react-native-reanimated-carousel';
 import { View } from 'tamagui';
 
 import { mainColor } from '../../../tamagui.config';
 
+const data = [
+  'https://s3.eu-central-1.amazonaws.com/different.dev/PqO9YhJ4SvmVIIV6GINY7w-0000000028:w=1000&h=1000',
+  'https://s3.eu-central-1.amazonaws.com/different.dev/PqO9YhJ4SvmVIIV6GINY7w-0000000029:w=4608&h=3456',
+  'https://s3.eu-central-1.amazonaws.com/different.dev/PqO9YhJ4SvmVIIV6GINY7w-0000000030:w=4608&h=3456',
+  'https://s3.eu-central-1.amazonaws.com/different.dev/PqO9YhJ4SvmVIIV6GINY7w-0000000031:w=4608&h=3456',
+  'https://s3.eu-central-1.amazonaws.com/different.dev/PqO9YhJ4SvmVIIV6GINY7w-0000000032:w=3456&h=4608',
+  'https://s3.eu-central-1.amazonaws.com/different.dev/Ry4oHWJ8TrqgphwAyBq0_w-0000000045:w=1278&h=1278',
+  'https://s3.eu-central-1.amazonaws.com/different.dev/Ry4oHWJ8TrqgphwAyBq0_w-0000000048:w=1278&h=1278',
+  'https://s3.eu-central-1.amazonaws.com/different.dev/Ry4oHWJ8TrqgphwAyBq0_w-0000000049:w=1278&h=1278',
+];
 export default function ListingPage() {
-  const width = Dimensions.get('window').width;
+  const screenWidth = Dimensions.get('window').width;
   const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
     <View className="flex-1">
       <Carousel
         loop
-        width={width}
-        height={width}
-        data={[
-          'https://media-assets.grailed.com/prd/listing/temp/e6248b8b4d964283900d1f695ab31901?h=1400&fit=clip&q=20&auto=format',
-          'https://s3.eu-central-1.amazonaws.com/different.dev/PqO9YhJ4SvmVIIV6GINY7w-0000000029:w=4608&h=3456',
-          'https://s3.eu-central-1.amazonaws.com/different.dev/PqO9YhJ4SvmVIIV6GINY7w-0000000030:w=4608&h=3456',
-          'https://s3.eu-central-1.amazonaws.com/different.dev/PqO9YhJ4SvmVIIV6GINY7w-0000000031:w=4608&h=3456',
-          'https://s3.eu-central-1.amazonaws.com/different.dev/PqO9YhJ4SvmVIIV6GINY7w-0000000032:w=3456&h=4608',
-          'https://s3.eu-central-1.amazonaws.com/different.dev/Ry4oHWJ8TrqgphwAyBq0_w-0000000045:w=1278&h=1278',
-          'https://s3.eu-central-1.amazonaws.com/different.dev/Ry4oHWJ8TrqgphwAyBq0_w-0000000048:w=1278&h=1278',
-          'https://s3.eu-central-1.amazonaws.com/different.dev/Ry4oHWJ8TrqgphwAyBq0_w-0000000049:w=1278&h=1278',
-        ]}
+        width={screenWidth}
+        height={screenWidth}
+        data={data}
         scrollAnimationDuration={300}
         onScrollEnd={(i) => setCurrentIndex(i)}
-        renderItem={({ item }) => <Image className="flex-1" source={{ uri: item }} alt="item" />}
+        renderItem={({ item }) => (
+          <Pinchable maximumZoomScale={6} className="flex-1">
+            <Image className="flex-1" source={{ uri: item }} alt="item" />
+          </Pinchable>
+        )}
       />
 
       <View className="absolute w-full">
