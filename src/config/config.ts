31f9d47a69bece55218765b65ff5ef4c132650f3
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-02-11 22:48:28 +0200

    Small config improvement

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,39 +1,26 @@
 import { isAndroid } from '../utils/platform';
 
+const additionalSearchParameters = { query_by: 'title,designer' };
+
 export const config = {
   local: {
     typesense: {
       server: {
-        nodes: [
-          {
-            host: isAndroid ? '192.168.1.117' : '127.0.0.1',
-            port: 8108,
-            protocol: 'http',
-          },
-        ],
+        nodes: [{ host: isAndroid ? '192.168.1.117' : '127.0.0.1', port: 8108, protocol: 'http' }],
         apiKey: 'xyz',
       },
-      additionalSearchParameters: {
-        query_by: 'title,designer',
-        filter_by: 'status:available',
-      },
+      additionalSearchParameters,
     },
     wsDomain: 'ws://localhost:8000',
   },
   production: {
     typesense: {
       server: {
-        nodes: [
-          {
-            host: 'o2by0uh958xjn1igp-1.a1.typesense.net',
-            port: 443,
-            protocol: 'https',
-          },
-        ],
+        nodes: [{ host: 'o2by0uh958xjn1igp-1.a1.typesense.net', port: 443, protocol: 'https' }],
         apiKey: 'LStsJHZnSoNFwVXzyfJU3D9xxvvjQcyg',
       },
-      additionalSearchParameters: { query_by: 'title,designer' },
+      additionalSearchParameters,
     },
-    wsDomain: 'wss://api.different-marketplace.com',
+    wsDomain: 'wss://api.different.to',
   },
 };
