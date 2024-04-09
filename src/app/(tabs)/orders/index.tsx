30fb127a8faf1e175e57590d63ab67d6c6f6af63
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-04-09 22:59:18 +0300

    Add order tabs

--- src/app/(tabs)/orders/index.tsx
+++ src/app/(tabs)/orders/index.tsx
@@ -1,27 +1,68 @@
 import { useQuery } from '@tanstack/react-query';
-import { Link } from 'expo-router';
-import { Text } from 'react-native';
-import { View } from 'tamagui';
+import { mainColor } from 'constants/colors';
+import * as React from 'react';
+import { Text, View, useWindowDimensions } from 'react-native';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 import { fetcher } from 'utils/fetcher';
 
-export default function OrdersScreen() {
-  const { data, isLoading } = useQuery({
+const BuyOrders = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Buy orders</Text>
+    </View>
+  );
+};
+
+const SellOrders = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Sell orders</Text>
+    </View>
+  );
+};
+
+const renderScene = SceneMap({
+  first: BuyOrders,
+  second: SellOrders,
+});
+
+export default function TabViewExample() {
+  const { isLoading } = useQuery({
     queryKey: ['orders'],
     queryFn: () => fetcher({ route: '/order/getMany' }),
   });
+
+  const layout = useWindowDimensions();
+  const [index, setIndex] = React.useState(0);
+
   if (isLoading) return null;
-  if (!data) return null;
 
   return (
-    <View>
-      <Text>orders tab</Text>
-      <Link
-        href={{
-          pathname: '/orders/order',
-          params: { orderId: '98c140ec-29a1-4f91-9fdd-766920864eeb' },
-        }}>
-        go to order
-      </Link>
-    </View>
+    <TabView
+      renderTabBar={(props) => (
+        <TabBar
+          {...props}
+          labelStyle={{
+            color: 'black',
+            textTransform: 'capitalize',
+            fontSize: 16,
+          }}
+          inactiveColor="#666666"
+          pressOpacity={0.5}
+          indicatorStyle={{ backgroundColor: mainColor }}
+          style={{ backgroundColor: 'white' }}
+        />
+      )}
+      navigationState={{
+        index,
+        routes: [
+          { key: 'first', title: 'Покупки' },
+          { key: 'second', title: 'Продажі' },
+        ],
+      }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+    />
   );
 }
