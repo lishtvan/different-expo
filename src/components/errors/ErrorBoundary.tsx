ea5036b86f028127fbfafafdc256f3a5c0f64627
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-06-05 15:07:25 +0300

    Implement no internet error

--- src/components/errors/ErrorBoundary.tsx
+++ src/components/errors/ErrorBoundary.tsx
@@ -1,7 +1,10 @@
+import { Ionicons } from '@expo/vector-icons';
+import { onlineManager } from '@tanstack/react-query';
 import { ErrorBoundaryProps } from 'expo-router';
 import * as WebBrowser from 'expo-web-browser';
+import { useState } from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
-import { Button, TamaguiProvider, Theme } from 'tamagui';
+import { Button, Spinner, TamaguiProvider, Theme } from 'tamagui';
 
 import tamaguiConfig from '../../../tamagui.config';
 
@@ -59,11 +62,59 @@ const InternalError = (props: ErrorBoundaryProps) => {
   );
 };
 
+const NoInternetError = (props: ErrorBoundaryProps) => {
+  const [checking, setChecking] = useState(false);
+  const checkConnection = async () => {
+    setChecking(true);
+    try {
+      await fetch('https://jsonplaceholder.typicode.com/todos/1');
+      props.retry();
+      setTimeout(() => {
+        onlineManager.setOnline(true);
+      }, 1000);
+    } catch {
+      setTimeout(() => {
+        setChecking(false);
+      }, 1000);
+    }
+  };
+
+  return (
+    <SafeAreaView className="flex-1 items-center">
+      <View className="rounded-3xl bg-red-100 px-4 py-1.5">
+        <Text className="text-lg font-semibold">Немає мережі</Text>
+      </View>
+      <View className="flex-1 flex-row items-center justify-center gap-x-2 px-5">
+        <Button
+          iconAfter={
+            checking ? (
+              <Spinner />
+            ) : (
+              <Ionicons name="reload-sharp" style={{ marginTop: 2 }} size={20} />
+            )
+          }
+          onPress={checkConnection}
+          size="$5"
+          fontSize="$6"
+          borderRadius="$main">
+          Перевірити з&apos;єднання
+        </Button>
+      </View>
+    </SafeAreaView>
+  );
+};
+
 export function ErrorBoundary(props: ErrorBoundaryProps) {
   return (
     <TamaguiProvider config={tamaguiConfig}>
       <Theme name="light">
-        {props.error.cause === 404 ? <NotFoundError {...props} /> : <InternalError {...props} />}
+        {props.error.message === 'Network request failed' ? (
+          <NoInternetError {...props} />
+        ) : props.error.cause === 404 ? (
+          <NotFoundError {...props} />
+        ) : (
+          <InternalError {...props} />
+        )}
       </Theme>
     </TamaguiProvider>
   );
