a3494c30b96e173c4b2a639136458a367b61b151
Sergey Lishtvan <sergey.lishtvan01@gmail.com>
2024-10-12 01:06:57 +0300

    Remove no internet error (EAS UPDATE)

---
+++ README.md
@@ -0,0 +1,6 @@
+## EAS Update:
+
+eas update --channel production --message $ --platform ios
+eas update --channel production --message $ --platform android
+eas update --channel production --message $
+eas update --clear-cache --channel production --message
--- src/components/errors/ErrorBoundary.tsx
+++ src/components/errors/ErrorBoundary.tsx
@@ -1,10 +1,7 @@
-import { Ionicons } from '@expo/vector-icons';
-import { onlineManager, useQueryClient, useQueryErrorResetBoundary } from '@tanstack/react-query';
 import { ErrorBoundaryProps } from 'expo-router';
 import * as WebBrowser from 'expo-web-browser';
-import { useState } from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
-import { Button, Spinner, TamaguiProvider, Theme } from 'tamagui';
+import { Button, TamaguiProvider, Theme } from 'tamagui';
 
 import tamaguiConfig from '../../../tamagui.config';
 
@@ -63,62 +60,11 @@ const InternalError = (props: ErrorBoundaryProps) => {
   );
 };
 
-const NoInternetError = (props: ErrorBoundaryProps) => {
-  const [checking, setChecking] = useState(false);
-  const errorBoundary = useQueryErrorResetBoundary();
-  const queryClient = useQueryClient();
-  const checkConnection = async () => {
-    setChecking(true);
-    try {
-      // LOW_PRIO: refactor this to net info
-      await fetch('https://jsonplaceholder.typicode.com/todos/1');
-      errorBoundary.reset();
-      queryClient.clear();
-      onlineManager.setOnline(true);
-      props.retry();
-    } catch {
-      setTimeout(() => {
-        setChecking(false);
-      }, 1000);
-    }
-  };
-
-  return (
-    <SafeAreaView className="flex-1 items-center">
-      <View className="mt-10 rounded-3xl bg-red-100 px-4 py-1.5">
-        <Text className="text-lg font-semibold">Немає мережі</Text>
-      </View>
-      <View className="flex-1 flex-row items-center justify-center gap-x-2 px-5">
-        <Button
-          iconAfter={
-            checking ? (
-              <Spinner />
-            ) : (
-              <Ionicons name="reload-sharp" style={{ marginTop: 2 }} size={20} />
-            )
-          }
-          onPress={checkConnection}
-          size="$5"
-          fontSize="$6"
-          borderRadius="$main">
-          Перевірити з&apos;єднання
-        </Button>
-      </View>
-    </SafeAreaView>
-  );
-};
-
 export function ErrorBoundary(props: ErrorBoundaryProps) {
   return (
     <TamaguiProvider config={tamaguiConfig}>
       <Theme name="light">
-        {props.error.message === 'Network request failed' ? (
-          <NoInternetError {...props} />
-        ) : props.error.cause === 404 ? (
-          <NotFoundError {...props} />
-        ) : (
-          <InternalError {...props} />
-        )}
+        {props.error.cause === 404 ? <NotFoundError {...props} /> : <InternalError {...props} />}
       </Theme>
     </TamaguiProvider>
   );
